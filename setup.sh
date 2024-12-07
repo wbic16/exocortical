d03e4fd693849beb1991f6ad45acc3266511968c
Will Bickford <wbic16@gmail.com>
2024-12-06 21:36:47 -0600

    Baseline exocortical node

---
+++ setup.sh
@@ -0,0 +1,11 @@
+#!/bin/bash
+
+# packages
+sudo apt install git -y
+sudo apt install zram-config -y
+sudo apt install build-essential -y
+sudo apt install vim -y
+sudo apt install htop -y
+
+# system config
+git config --global init.defaultBranch exo
