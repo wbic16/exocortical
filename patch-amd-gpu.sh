20737c78f0290b76e43267de1a2336717748bb8b
Will Bickford <wbic16@gmail.com>
2025-01-05 21:34:45 -0600

    Steps to configure rocm on Ubuntu 24.04 LTS

--- patch-amd-gpu.sh
+++ patch-amd-gpu.sh
@@ -1,6 +1,19 @@
 #!/bin/sh
+# https://rocm.docs.amd.com/projects/install-on-linux/en/latest/install/native-install/ubuntu.html
 wget -qO - https://repo.radeon.com/rocm/rocm.gpg.key | sudo apt-key add -
-echo 'deb [arch=amd64] https://repo.radeon.com/rocm/apt/5.5 jammy main' |sudo tee /etc/apt/sources.list.d/rocm.list
+
+sudo mkdir --parents --mode=0755 /etc/apt/keyrings
+wget https://repo.radeon.com/rocm/rocm.gpg.key -O - | gpg --dearmor | sudo tee /etc/apt/keyrings/rocm.gpg > /dev/null
+
 sudo apt-get update
-sudo apt-get install rocm-dkms
-sudo add-apt-repository -y -s deb http://security.ubuntu.com/ubuntu jammy main universe
+sudo apt-get install rocm-dkms -y
+
+echo "deb [arch=amd64 signed-by=/etc/apt/keyrings/rocm.gpg] https://repo.radeon.com/amdgpu/6.3.1/ubuntu noble main" | sudo tee /etc/apt/sources.list.d/amdgpu.list
+sudo apt update -y
+
+echo "deb [arch=amd64 signed-by=/etc/apt/keyrings/rocm.gpg] https://repo.radeon.com/rocm/apt/6.3.1 noble main" | sudo tee --append /etc/apt/sources.list.d/rocm.list
+echo -e 'Package: *\nPin: release o=repo.radeon.com\nPin-Priority: 600' | sudo tee /etc/apt/preferences.d/rocm-pin-600
+sudo apt update -y
+
+sudo apt install amdgpu-dkms -y
+sudo apt install rocm -y
