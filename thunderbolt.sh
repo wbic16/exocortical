c44787f5187fef0375541af4fd0f23b607c563c7
Will Bickford <wbic16@gmail.com>
2025-02-02 23:24:45 -0600

    tweaks to control tb0 and tb1 more effectively

--- thunderbolt.sh
+++ thunderbolt.sh
@@ -10,14 +10,19 @@ if [ -f .tb-switch ]; then
   TB_SECOND="tb0"
 fi
 FIRST_NETWORK=43
-SECOND_NETWORK=44
+SECOND_NETWORK=43
 if [ -f .tb0-network ]; then
   FIRST_NETWORK=`cat .tb0-network`
 fi
 if [ -f .tb1-network ]; then
   SECOND_NETWORK=`cat .tb1-network`
 fi
-sudo nmcli connection add type ethernet ifname thunderbolt0 con-name $TB_FIRST ipv4.method manual ipv4.addresses 10.42.$FIRST_NETWORK.$ID/24 ipv4.gateway 10.42.$FIRST_NETWORK.$ID
+FIRST_ID=$ID
+#TODO: figure out why bridging doesn't work
+#sudo brctl addbr br0 >/dev/null 2>&1
+sudo nmcli connection add type ethernet ifname thunderbolt0 con-name $TB_FIRST ipv4.method manual ipv4.addresses 10.42.$FIRST_NETWORK.$ID/24 ipv4.gateway 10.42.$FIRST_NETWORK.$FIRST_ID
 ID=$(($ID + 1))
-sudo nmcli connection add type ethernet ifname thunderbolt1 con-name $TB_SECOND ipv4.method manual ipv4.addresses 10.42.$SECOND_NETWORK.$ID/24 ipv4.gateway 10.42.$SECOND_NETWORK.$ID
+sudo nmcli connection add type ethernet ifname thunderbolt1 con-name $TB_SECOND ipv4.method manual ipv4.addresses 10.42.$SECOND_NETWORK.$ID/24 ipv4.gateway 10.42.$SECOND_NETWORK.$FIRST_ID
 sudo netplan apply
+#sudo brctl addif br0 tb0
+#sudo brctl addif br0 tb1
