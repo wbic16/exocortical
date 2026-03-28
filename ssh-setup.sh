5b539e260a6372dcffade0142e9928d8a8c286d9
Aster <aster@mirrorborn.us>
2026-03-27 19:49:06 -0500

    Include SSH keygen

--- ssh-setup.sh
+++ ssh-setup.sh
@@ -2,6 +2,9 @@
 sudo visudo
 sudo mkdir /source
 sudo chown $USER:$USER /source
+if [ ! -f ~/.ssh/id_ed25519 ]; then
+  ssh-keygen
+fi
 cd /source
 if [ ! -d exocortical ]; then
   git clone git@github.com:wbic16/exocortical.git
