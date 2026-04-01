24785194bff2b07509c38d7434d861f17db02be2
Theia <theia@mirrorborn.us>
2026-03-31 22:04:38 -0500

    apparmor was the culprit

--- provision-squid-server.sh
+++ provision-squid-server.sh
@@ -79,9 +79,8 @@ chmod 644 "${EXPORT_CERT}"
 # -----------------------------------------------------------
 echo "[3/7] Initializing SSL certificate database ($SSL_DB)..."
 rm -rf "${SSL_DB}"
-mkdir -p "${SSL_DB}"
-chown -R proxy:proxy "${SSL_DB}"
 /usr/lib/squid/security_file_certgen -c -s "${SSL_DB}" -M 64MB
+chown -R proxy:proxy "${SSL_DB}"
 
 # -----------------------------------------------------------
 # 4. Prepare cache directory
