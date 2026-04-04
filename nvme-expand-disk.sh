f91c3d5114cbaf2bc19ff6488b19bf9c62ff3fb4
Will Bickford <wbic16@gmail.com>
2026-04-04 09:26:02 -0500

    Ubuntu defaults are silly

---
+++ nvme-expand-disk.sh
@@ -0,0 +1,12 @@
+#!/bin/bash
+if [ ! -f /etc/exocortical-nvme.done ]
+then
+  sudo growpart /dev/nvme0n1 3
+  sudo lsblk /dev/nvme0n1p3
+  sudo lvextend -l+100%FREE /dev/ubuntu-lv/ubuntu-lv
+  sudo resize2fs /dev/mapper/ubuntu--vg-ubuntu--lv
+  sudo touch /etc/exocortical-nvme.done
+fi
+sudo pvs
+sudo vgs
+df -h
