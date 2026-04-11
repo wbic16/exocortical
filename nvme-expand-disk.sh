b1cec058acacc7dbb6d6b2a1ec42b53c3fd7c62f
Will Bickford <wbic16@gmail.com>
2026-04-11 10:29:34 -0500

    fix typo

--- nvme-expand-disk.sh
+++ nvme-expand-disk.sh
@@ -3,7 +3,7 @@ if [ ! -f /etc/exocortical-nvme.done ]
 then
   sudo growpart /dev/nvme0n1 3
   sudo lsblk /dev/nvme0n1p3
-  sudo lvextend -l+100%FREE /dev/ubuntu-lv/ubuntu-lv
+  sudo lvextend -l+100%FREE /dev/ubuntu-vg/ubuntu-lv
   sudo resize2fs /dev/mapper/ubuntu--vg-ubuntu--lv
   sudo touch /etc/exocortical-nvme.done
 fi
